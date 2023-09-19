b747d9416b3bbf2d7a394ff8d205d2a908f7f3d3
Oleksii Yaremenko <alex.g.yaremenko@gmail.com>
2023-09-19 12:20:16 +0200

    chore: roto vr plugin android studio project configuration fixes

---
+++ com.rotovr.android.bleplugin/app/src/main/java/com/rotovr/androidbleplugin/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rotovr.androidbleplugin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Roto VR Android Sample");
+    }
+}
