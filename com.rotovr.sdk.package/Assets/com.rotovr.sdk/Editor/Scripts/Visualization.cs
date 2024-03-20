9d87786ca4fcc8a23f1b7166fe5c78f84e17fd3d
Dmytro Ishchenko <madbytelabs@gmail.com>
2024-03-20 18:14:21 +0200

    chore: Add a chair visualisation and behaviour simulation without the chair

---
+++ com.rotovr.sdk.package/Assets/com.rotovr.sdk/Editor/Scripts/Visualization.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using RotoVR.SDK.Components;
+using RotoVR.SDK.Enum;
+using RotoVR.SDK.Model;
+using UnityEngine;
+
+namespace com.rotovr.sdk.Editor.Scripts
+{
+    public class Visualization : MonoBehaviour
+    {
+        RotoBehaviour m_Behaviour;
+        Coroutine m_RotationRoutine;
+        float m_TargetAngle;
+        RotoDataModel m_RotoModel;
+
+        void Awake()
+        {
+            m_Behaviour = FindObjectOfType<RotoBehaviour>();
+            if (m_Behaviour == null)
+                return;
+
+            m_RotoModel = new RotoDataModel();
+            m_Behaviour.OnConnectionStatusChanged += OnConnectionHandler;
+            m_Behaviour.OnDataChanged += OnDataHandler;
+        }
+
+        void OnDestroy()
+        {
+            if (m_Behaviour == null)
+                return;
+            m_Behaviour.OnConnectionStatusChanged -= OnConnectionHandler;
+            m_Behaviour.OnDataChanged -= OnDataHandler;
+        }
+
+        void OnDataHandler(RotoDataModel model)
+        {
+            m_RotoModel = model;
+        }
+
+        void OnConnectionHandler(ConnectionStatus status)
+        {
+            switch (status)
+            {
+                case ConnectionStatus.Connected:
+                    if (m_RotationRoutine == null)
+                        m_RotationRoutine = StartCoroutine(RotationRoutine());
+                    break;
+                case ConnectionStatus.Disconnected:
+                    if (m_RotationRoutine != null)
+                    {
+                        StopCoroutine(m_RotationRoutine);
+                        m_RotationRoutine = null;
+                    }
+
+                    break;
+            }
+        }
+
+        IEnumerator RotationRoutine()
+        {
+            while (true)
+            {
+                yield return null;
+                var desiredRotQ = Quaternion.Euler(transform.localEulerAngles.x, m_RotoModel.Angle,
+                    transform.localEulerAngles.z);
+                transform.localRotation = Quaternion.Lerp(transform.localRotation, desiredRotQ, Time.deltaTime * 10f);
+            }
+        }
+    }
+}
---
+++ com.rotovr.sdk.package/Assets/com.rotovr.sdk/Runtime/Common/Enum/BehaviourType.cs
@@ -0,0 +1,10 @@
+using System.Runtime.InteropServices;
+
+namespace RotoVR.SDK.Enum
+{
+    public enum BehaviourType
+    {
+        Runtime,
+        Simulation,
+    }
+}
